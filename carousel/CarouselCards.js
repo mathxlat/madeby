5c395412bdc848231d3d24e15978c1613ddabc53
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2021-07-14 01:57:42 -0300

    madeby - v2.0.1 - Agregado de Redux para el manejo de estado.

--- carousel/CarouselCards.js
+++ carousel/CarouselCards.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { View } from "react-native";
+import { useSelector } from 'react-redux'
 import Carousel from "react-native-snap-carousel";
 import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from "./CarouselCardItem";
-import { CATEGORIES } from "../services/categories";
 
 const CarouselCards = () => {
-    const [index, setIndex] = React.useState(0);
     const isCarousel = React.useRef(null);
+    const productCategories = useSelector(state => state.categories.categories)
+
 
     return (
     <View>
         <Carousel
         layout="default"
         ref={isCarousel}
-        data={CATEGORIES}
+        data={productCategories}
         renderItem={CarouselCardItem}
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
-        onSnapToItem={(index) => setIndex(index)}
         useScrollView={true}
         autoplay={true}
         autoplayInterval={10000}
